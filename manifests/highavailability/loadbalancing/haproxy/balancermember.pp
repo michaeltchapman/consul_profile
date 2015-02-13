dd07cbe3a55c3a7e12aacbe05b5f7b402a3fd674
Michael Chapman <woppin@gmail.com>
2015-02-14 02:16:54 +1100

    Support manual address override for members

--- manifests/highavailability/loadbalancing/haproxy/balancermember.pp
+++ manifests/highavailability/loadbalancing/haproxy/balancermember.pp
@@ -8,11 +8,12 @@ define consul_profile::highavailability::loadbalancing::haproxy::balancermember 
   $json_config_hash = hiera("haproxy::${datacenter}::${service}::${nn}::config_hash", false)
   if $json_config_hash {
     $config_hash = parsejson($json_config_hash)
-    if 'server' in $config_hash {
-      $options = $config_hash['server']
-    } else {
-      $options = []
-    }
+  } else {
+    $config_hash = {}
+  }
+
+  if 'server' in $config_hash {
+    $options = $config_hash['server']
   } else {
     $options = []
   }
@@ -26,7 +27,13 @@ define consul_profile::highavailability::loadbalancing::haproxy::balancermember 
   }
 
   if $balancemember {
-    $address   = $service_hash[$service][$nn]['Address']
+    # This can be used until consul adds support natively for multiple addresses
+    if 'address' in $config_hash {
+      $address   = $config_hash['address']
+    } else {
+      $address   = $service_hash[$service][$nn]['Address']
+    }
+
     $node  = $service_hash[$service][$nn]['Node']
     $port      = $service_hash[$service][$nn]['ServicePort']
 
