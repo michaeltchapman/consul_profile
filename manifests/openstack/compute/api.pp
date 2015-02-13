2ed0017378bdf673462e7e1b629749de356873d0
Michael Chapman <michchap@cisco.com>
2015-02-13 21:02:54 +1100

    Don't use profile for compute api but add fw

--- manifests/openstack/compute/api.pp
+++ manifests/openstack/compute/api.pp
@@ -1,4 +1,7 @@
-class consul_profile::openstack::compute::api {
+class consul_profile::openstack::compute::api(
+  $manage_firewall = true,
+  $firewall_extras = {}
+) {
   include ::consul_profile::openstack::compute
 
   if ! hiera('service_hash__haproxy::balanced__keystone_Address', false) {
@@ -8,7 +11,24 @@ class consul_profile::openstack::compute::api {
     }
   } else {
     Consul_profile::Discovery::Consul::Multidep <| title == 'novamultidep' |> {
-      includes +> ['::profile::openstack::compute::api', '::consul_profile::discovery::consul::compute_api']
+      includes +> ['::nova::api', '::consul_profile::discovery::consul::compute_api']
+    }
+
+    if $manage_firewall {
+      profile::firewall::rule { '231 compute api accept tcp':
+        port   => 8774,
+        extras => $firewall_extras
+      }
+
+      profile::firewall::rule { '232 compute api-ec2 accept tcp':
+        port   => 8773,
+        extras => $firewall_extras
+      }
+
+      profile::firewall::rule { '233 compute api-metadata accept tcp':
+        port   => 8775,
+        extras => $firewall_extras
+      }
     }
   }
 }
